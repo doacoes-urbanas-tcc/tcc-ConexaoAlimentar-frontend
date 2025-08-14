19c823c9c61b9799f616189d805ee6ae3d01c868
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-08-14 08:53:58 -0300

    fix: concerta o erro para baixar os relatórios do admin

--- js/relatorio-admin.js
+++ js/relatorio-admin.js
@@ -1,19 +1,22 @@
 function baixar(tipo, formato) {
   const token = localStorage.getItem("token");
-  let url = `https://conexao-alimentar.onrender.com/admin/relatorio/${formato}`;
+  let url = `https://conexao-alimentar.onrender.com/admin/relatorio/${formato}/${tipo}`;
 
   if (tipo === 'doacoes-mensais') {
     const mes = document.getElementById('mes').value;
     const ano = document.getElementById('ano').value;
-    url += `/doacoes-mensais?mes=${mes}&ano=${ano}`;  
-  } else {
-    url += `/${tipo}`;
+    url += `?mes=${mes}&ano=${ano}`;
   }
 
   fetch(url, {
     headers: { Authorization: `Bearer ${token}` }
   })
-  .then(res => res.blob())
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`Erro ${res.status}: ${res.statusText}`);
+    }
+    return res.blob();
+  })
   .then(blob => {
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
@@ -21,5 +24,6 @@ function baixar(tipo, formato) {
     document.body.appendChild(link);
     link.click();
     link.remove();
-  });
+  })
+  .catch(err => alert(`Falha ao baixar relatório: ${err.message}`));
 }
